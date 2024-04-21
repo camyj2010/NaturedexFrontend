6c67395f548add6791752e6882e83cdb1ff2217d
Cesar-velasquez117 <106538584+Cesar-velasquez117@users.noreply.github.com>
2024-04-21 16:20:59 -0500

    Design for the PopUp message to upload image completed

--- naturedex/src/components/FloatingButton.jsx
+++ naturedex/src/components/FloatingButton.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import FloatingButtonCSS from './FloatingButton.module.css'; // Importa el archivo CSS como módulo
+import React, {useState} from 'react';
+import FloatingButtonCSS from './FloatingButton.module.css';
+import PopUp from './PopUp';
 
 const FloatingButton = () => {
+  const [isPopUpOpen, setIsPopUpOpen] = useState(false);
+
+  const openPopUp = () => {
+    setIsPopUpOpen(true);
+  };
+
+  const closePopUp = () => {
+    setIsPopUpOpen(false);
+  };
+
   return (
-    <div className={FloatingButtonCSS.floatingButton}>
-      <svg  xmlns="http://www.w3.org/2000/svg"  width="28"  height="28"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-camera"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 7h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2" /><path d="M9 13a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /></svg>
+    <div>
+      <div className={FloatingButtonCSS.floatingButton} onClick={openPopUp}>
+        <svg  xmlns="http://www.w3.org/2000/svg"  width="28"  height="28"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-camera"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 7h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2" /><path d="M9 13a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /></svg>
+      </div>
+      <PopUp isOpen={isPopUpOpen} onClose={closePopUp}/>
     </div>
   );
 };
